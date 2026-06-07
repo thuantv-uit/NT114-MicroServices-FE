5449dc2707349d0d1fe2e75a0e505059b636bc15
thuantv-uit <tvt04102004@gmail.com>
2026-06-07 15:57:11 +0700

    feat(config): login by github

--- src/features/users/components/Login.js
+++ src/features/users/components/Login.js
@@ -34,10 +34,9 @@ const GoogleSVG = () => (
   </svg>
 );
 
-const FacebookSVG = () => (
-  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="18" height="18" rx="4" fill="#1877F2"/>
-    <path d="M12.5 11.5l.469-3H10V6.75c0-.82.402-1.62 1.693-1.62H13V2.7S11.808 2.5 10.67 2.5c-2.388 0-3.95 1.447-3.95 4.066V8.5H4v3h2.72V18h3.344v-6.5H12.5Z" fill="white"/>
+const GitHubSVG = () => (
+  <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 0C5.374 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0 1 12 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z"/>
   </svg>
 );
 
@@ -74,10 +73,15 @@ const Login = ({ setToken }) => {
     }
   };
 
-  // 🆕 Redirect thẳng sang backend → Google sẽ xử lý OAuth flow
   const handleGoogleLogin = () => {
     const baseUrl = process.env.REACT_APP_USER_SERVICE_URL.replace('/api/users', '');
     window.location.href = `${baseUrl}/auth/google`;
+  };
+
+  // 🆕 GitHub login
+  const handleGitHubLogin = () => {
+    const baseUrl = process.env.REACT_APP_USER_SERVICE_URL.replace('/api/users', '');
+    window.location.href = `${baseUrl}/auth/github`;
   };
 
   return (
@@ -124,7 +128,6 @@ const Login = ({ setToken }) => {
           <p className="login-card__subtitle">Sign in to your account</p>
 
           <form className="login-form" onSubmit={handleSubmit} noValidate>
-            {/* Email */}
             <div className="login-field">
               <label className="login-field__label" htmlFor="email">
                 Email <span className="login-field__req">*</span>
@@ -141,7 +144,6 @@ const Login = ({ setToken }) => {
               {errors.email && <p className="login-field__error">{errors.email}</p>}
             </div>
 
-            {/* Password */}
             <div className="login-field">
               <label className="login-field__label" htmlFor="password">
                 Password <span className="login-field__req">*</span>
@@ -172,21 +174,18 @@ const Login = ({ setToken }) => {
             </button>
           </form>
 
-          {/* Divider */}
           <div className="login-divider">or continue with</div>
 
-          {/* Social */}
           <div className="login-social-row">
-            {/* 🆕 Dùng onClick thay vì button thường */}
             <button className="login-social-btn" onClick={handleGoogleLogin}>
               <GoogleSVG /> Google
             </button>
-            <button className="login-social-btn">
-              <FacebookSVG /> Facebook
+            {/* 🆕 GitHub button */}
+            <button className="login-social-btn" onClick={handleGitHubLogin}>
+              <GitHubSVG /> GitHub
             </button>
           </div>
 
-          {/* Sign up */}
           <div className="login-signup-row">
             Don't have an account? <Link to="/register">Sign up</Link>
           </div>
